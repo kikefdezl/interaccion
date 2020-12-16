2c0659dd43d265ce61790500fdcc5d6187b9368f
DaniGarciaLopez <d.garcialopez@hotmail.com>
2020-12-16 20:54:20 +0100

    Add multiplicador_nodo

---
+++ scripts/matematico_nodo.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+from interaccion.srv import multiplicador
+import rospy
+
+def multiply(req):
+    multiplication=int(req.entrada*2)
+    rospy.loginfo("Multiplicando: %s x 2 = %s"%(req.entrada, multiplication))
+    return multiplication
+
+def multiply_server():
+    rospy.init_node('matematico_nodo')
+    s = rospy.Service('multiplicador', multiplicador, multiply)
+    rospy.loginfo("Listo para multiplicar...")
+    rospy.spin()
+
+if __name__ == "__main__":
+    multiply_server()
